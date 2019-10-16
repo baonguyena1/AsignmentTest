a0aae937067a4cd5cff702b7f4231d0ddd632852
Bao Nguyen <bao.nguyen@mobility-x.com>
2019-10-16 09:58:41 +0700

    Initial Commit

---
+++ TestAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAssignment
+//
+//  Created by Bao Nguyen on 10/16/19.
+//  Copyright © 2019 Bao Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
